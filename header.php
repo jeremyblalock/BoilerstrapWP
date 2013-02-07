72a23cfcf847529392314b77ddaa84913f89df71
Alex Muraro <akmuraro@gmail.com>
2013-02-07 12:28:55 +0100

    first commit, importing from my fork of html5-for-wordpress

---
+++ header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5_Boilerplate
+ */
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+       Remove this if you use the .htaccess -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    
+    <meta name="title" content="<?php wp_title('&laquo;', true, 'right'); ?> | <?php bloginfo('name'); ?>">
+    <meta name="description" content="<h1><?php bloginfo('name'); ?></h1> | <?php bloginfo('description'); ?>">
+    
+    <meta name="viewport" content="width=device-width">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <?php versioned_stylesheet($GLOBALS["TEMPLATE_RELATIVE_URL"]."html5-boilerplate/css/normalize.css") ?>
+    <?php versioned_stylesheet($GLOBALS["TEMPLATE_RELATIVE_URL"]."html5-boilerplate/css/main.css") ?>
+    
+    <!-- Wordpress Templates require a style.css in theme root directory -->
+    <?php versioned_stylesheet($GLOBALS["TEMPLATE_RELATIVE_URL"]."style.css") ?>
+    
+    <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+    <?php versioned_javascript($GLOBALS["TEMPLATE_RELATIVE_URL"]."html5-boilerplate/js/vendor/modernizr.min.js") ?>
+
+    <!-- Wordpress Head Items -->
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <?php wp_head(); ?>
+
+</head>
+<body <?php body_class(); ?>>
+  <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+  <![endif]-->
+
+  <div id="container">
+    <header role="banner">
+      <h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
+      <p class="description"><?php bloginfo('description'); ?></p>
+    </header>
